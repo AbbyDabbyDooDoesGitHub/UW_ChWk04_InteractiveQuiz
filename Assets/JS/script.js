a1077ab100daa982050393abec0f12b28712a5b2
Abigail Douglas <eburgabby@aol.com>
2022-07-22 23:15:54 -0700

    generateq function

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -5,6 +5,23 @@
 //     return list[Math.floor((Math.random()*list.length))]; 
 // }
 
+var q1_H = "Question 1 example question wow so question-y?";
+var q1_A = ["answer1","answer2","answer3","answer4"];
+var q2_H = "Question 2 example question wow so question-y?";
+var q2_A = ["answer1","answer2","answer3","answer4"];
+var q3_H = "Question 3 example question wow so question-y?";
+var q3_A = ["answer1","answer2","answer3","answer4"];
+var q4_H = "Question 4 example question wow so question-y?";
+var q4_A = ["answer1","answer2","answer3","answer4"];
+var q5_H = "Question 5 example question wow so question-y?";
+var q5_A = ["answer1","answer2","answer3","answer4"];
+
+var q_H1   = document.querySelector("#questionH1");
+var q_Btn1 = document.querySelector("#questionBtn1");
+var q_Btn2 = document.getElementById("#questionBtn2");
+var q_Btn3 = document.getElementById("#questionBtn3");
+var q_Btn4 = document.getElementById("#questionBtn4");
+
 // FUNCTION TO SHUFFLE AN ARRAY
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -25,27 +42,23 @@ function shuffle(array) {
 
 }
 
-function generateQuestion {
-    var q1_header = "Question 1 example question wow so question-y?";
-    var q1_answers = ["answer1","answer2","answer3","answer4"];
+function generateQuestion (q_question,q_answerArray) {
+    shuffle(q_answerArray);
+    q_H1.innerHTML   = q_question;
+    q_Btn1.innerHTML = q_answerArray[0];
+    q_Btn2.innerHTML = q_answerArray[1];
+    q_Btn3.innerHTML = q_answerArray[2];
+    q_Btn4.innerHTML = q_answerArray[3];
     
-    var q2_header = "Question 1 example question wow so question-y?";
-    var q2_answers = ["answer1","answer2","answer3","answer4"];
-
-    var q3_header = "Question 1 example question wow so question-y?";
-    var q3_answers = ["answer1","answer2","answer3","answer4"];
-
-    var q4_header = "Question 1 example question wow so question-y?";
-    var q4_answers = ["answer1","answer2","answer3","answer4"];
-
-    var q5_header = "Question 1 example question wow so question-y?";
-    var q5_answers = ["answer1","answer2","answer3","answer4"];
-
-    shuffle(q1_answers);
-    shuffle(q2_answers);
-    shuffle(q3_answers);
-    shuffle(q4_answers);
-    shuffle(q5_answers);
 }
 
-
+    // function startGame() {
+    //     //hide the main page
+    //     startEl.classList.add("hide");
+    //     //show the quiz start screen
+    //     answersEl.classList.remove("hide");
+    //     quoteEl.classList.remove("hide");
+    //     streakBarEl.classList.remove("hide");
+    
+    // };
+    
