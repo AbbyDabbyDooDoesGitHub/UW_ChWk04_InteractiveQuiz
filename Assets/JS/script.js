b7aec61f9a97883491599aff10bb585f765f0820
Abigail Douglas <EburgAbby@aol.com>
2022-07-28 01:13:55 -0700

    added comments to index

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -397,14 +397,3 @@ function validateEntry() {
 
 }
 
-// // // Add data
-// // localStorage.setItem('myCar', 'Tesla');
-
-// // // Read data
-// // const car = localStorage.getItem('myCar');
-
-// // // Remove specific data
-// // localStorage.removeItem('myCar');
-
-// // // Remove all data
-// // localStorage.clear();
